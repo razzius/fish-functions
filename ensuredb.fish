fb5b2361c7f09a9b41c83d850b01b7498e7be63c
Razzi Abuissa <razzi53@gmail.com>
2017-05-01 09:08:49 -0700

    Update ensuredb

--- ensuredb.fish
+++ ensuredb.fish
@@ -1,14 +1,5 @@
 function ensuredb --argument db
-  if [ (count (psql -Atqc "\list $db")) -eq 1 ]
-    echo dropping
-    if not dropdb $db
-      echo force quitting
-      echo 'SELECT pg_terminate_backend(pg_stat_activity.pid)
-            FROM pg_stat_activity
-            WHERE datname = current_database()
-            AND pid <> pg_backend_pid();' | psql -d $db > /dev/null
-      dropdb $db
-    end
-  end
+	psql -d $db -c "select pg_terminate_backend(pid) from pg_stat_activity where pid <> pg_backend_pid() and datname = '$db'" >/dev/null
+  dropdb $db >/dev/null
   createdb $db
 end
