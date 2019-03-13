1fe8baa119af0641801a3f01c7ed006f034e3948
Razzi Abuissa <razzi.abuissa@goodeggs.com>
2019-03-13 10:25:05 -0700

    Emacs mkdirp

--- functions/emacs.fish
+++ functions/emacs.fish
@@ -1,6 +1,7 @@
-# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.KZqQ4d/emacs.fish @ line 2
+# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.8v7STj/emacs.fish @ line 2
 function emacs
 	if not [ -e $argv ]
+    mkdir -p (dirname $argv)
     touch $argv
   end
 	open -a /Applications/Emacs.app/ $argv
