a693c395615ef93f59aa0803148003a8bd1d581c
Razzi Abuissa <razzi53@gmail.com>
2019-08-01 13:39:42 -0700

    Update functions/project-replace.fish

--- functions/project-replace.fish
+++ functions/project-replace.fish
@@ -1,3 +1,6 @@
-function project-replace --argument a b
-	find . -print -exec sed -i'' "s/$a/$b/g" {} \;
+# Defined in /var/folders/5r/x8hb9kbd5751zn4pz9yq24s80000gn/T//fish.vyyUG0/project-replace.fish @ line 2
+function project-replace --argument a b extension
+	for f in (fd $extension)
+	  gsed -i'' "s/$a/$b/g" $f
+	end
 end
