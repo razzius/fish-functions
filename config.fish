81c749f44f4c914f4259c98f33341e1bec23aaec
Razzi Abuissa <razzi53@gmail.com>
2019-08-01 13:54:51 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,18 @@
+if not status is-interactive
+  exit
+end
+
+if not test -n "$TMUX"
+  tmux
+end
+
+for f in ~/.config/fish/functions/private/*
+  source $f
+end
+
+bind \e\[1\;5A history-token-search-backward
+bind \e\[1\;5B history-token-search-forward
+
 bind \ct transpose-chars
 
 # emacs ansi-term support
@@ -5,12 +20,9 @@ if test -n "$EMACS"
   set -x TERM eterm-color
 end
 
-# this function may be required
-function fish_title
-  true
-end
-
 source ~/.profile
+source ~/.fish_abbrs.fish
 
-eval (python -m virtualfish)
-status --is-interactive; and source (pyenv init -|psub)
+source (pyenv init -|psub)
+eval (python3 -m virtualfish)
+
