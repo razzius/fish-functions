71ecbd4f55e0daf5ca431601ccdc2375c70c9e1e
Razzi Abuissa <razzi53@gmail.com>
2019-08-26 12:00:18 -0700

    Update functions/last-function-name.fish

--- functions/last-function-name.fish
+++ functions/last-function-name.fish
@@ -1,3 +1,4 @@
+# Defined in /var/folders/5r/x8hb9kbd5751zn4pz9yq24s80000gn/T//fish.3tFFdX/last-function-name.fish @ line 2
 function last-function-name --argument index
-	history --prefix func | awk 'NF>1' | row (default $index 1) | replace '-s' '' | coln 2
+	history --prefix func | replace '-s' '' | awk 'NF>1' | row (default $index 1) | coln 2
 end
