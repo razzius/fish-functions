5cc47612f6ae04c567ef5547384a9f7e7d8a61e8
Razzi Abuissa <razzi53@gmail.com>
2019-08-28 11:41:18 -0700

    Update functions/gc.fish

--- functions/gc.fish
+++ functions/gc.fish
@@ -1,8 +1,9 @@
-# Defined in /var/folders/5r/x8hb9kbd5751zn4pz9yq24s80000gn/T//fish.SUeeSK/gc.fish @ line 2
 function gc
     if [ -z "$argv" ]
         if test (git new-files | wc -l | tr -d ' ') = 1
             git commit -m "Add "(git staged-files)
+        else if test (git deleted-files | wc -l | tr -d ' ') = 1
+            git commit -m "Delete "(git deleted-files)
         else if test (git staged-files | wc -l | tr -d ' ') = 1
             git commit -m "Update "(git staged-files)
         else
