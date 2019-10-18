e598a92ea857f0119f6256ff0b2c050a21c402d2
Razzi Abuissa <razzi53@gmail.com>
2019-10-18 14:00:59 -0700

    Updates to gc and rg

--- functions/rg.fish
+++ functions/rg.fish
@@ -1,8 +1,8 @@
-# Defined in /var/folders/5r/x8hb9kbd5751zn4pz9yq24s80000gn/T//fish.tUijyN/rg.fish @ line 2
+# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.Uhljhj/rg.fish @ line 2
 function rg
-    if test (count (echo $argv | grep -v '-')) -gt 1 && test ! -e $argv[-1]
-        command rg "$argv"
-    else
-        command rg $argv
-    end
+	if test (count (echo $argv | sed s/-// | string split ' ')) -gt 1 && test ! -e $argv[-1]
+    command rg "$argv"
+  else
+    command rg $argv
+  end
 end
