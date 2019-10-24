da89baf892c97ed85b5f0b1720afb4bcb1981d01
Razzi Abuissa <razzi53@gmail.com>
2019-10-24 12:39:58 -0700

    Update functions/start-jupyter.fish

--- functions/start-jupyter.fish
+++ functions/start-jupyter.fish
@@ -1,4 +1,5 @@
-# Defined in /var/folders/5r/x8hb9kbd5751zn4pz9yq24s80000gn/T//fish.b7YEZd/start-jupyter.fish @ line 2
+# Defined in /var/folders/5r/x8hb9kbd5751zn4pz9yq24s80000gn/T//fish.jIvNBi/start-jupyter.fish @ line 2
 function start-jupyter
-    launchctl start local.jupyter.notebook
+	launchctl start local.jupyter.notebook
+  echo "rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 8888" | sudo pfctl -ef - ^ /dev/null
 end
