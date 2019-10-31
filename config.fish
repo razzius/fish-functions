5b127e887045e29f20a0cc8bf64006b100696764
Razzi Abuissa <razzi53@gmail.com>
2019-10-30 23:18:19 -0700

    Remove tools I am deprecating: tmux, ansi-term, pyenv, virtualfish

--- config.fish
+++ config.fish
@@ -2,29 +2,16 @@ if not status is-interactive
     exit
 end
 
-if not test -n "$TMUX"
-    tmux
-end
-
 for f in ~/.config/fish/functions/private/*
     source $f
 end
 
 bind \e\[1\;5A history-token-search-backward
 bind \e\[1\;5B history-token-search-forward
-
 bind \ct transpose-chars
-
-# emacs ansi-term support
-if test -n "$EMACS"
-    set -x TERM eterm-color
-end
 
 source ~/.profile
 source ~/.fish_abbrs.fish
-
-source (pyenv init -|psub)
-eval (python3 -m virtualfish)
 
 function postexec_source_profile --on-event fish_postexec
   if test "$argv" = "vi ~/.profile"
