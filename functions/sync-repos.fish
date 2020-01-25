d427929d9dec6648358968ea58df33b41f577461
Razzi Abuissa <razzi53@gmail.com>
2020-01-24 18:44:09 -0800

    Update functions/sync-repos.fish

--- functions/sync-repos.fish
+++ functions/sync-repos.fish
@@ -1,8 +1,10 @@
+# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.umzv79/sync-repos.fish @ line 2
 function sync-repos
 	for repo in ~/.config/fish ~/.spacemacs.d ~/.dotfiles ~/.password-store/
 echo $repo
 git -C $repo stat
 git -C $repo pull
 git -C $repo push
+echo
 end
 end
