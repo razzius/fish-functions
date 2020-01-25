21b3ae41cd51f972a4cfbef9df6e40ed0c0fc412
Razzi Abuissa <razzi53@gmail.com>
2020-01-24 18:36:59 -0800

    New functions and automatically source .profile and flush dns after editing /etc/hosts

--- config.fish
+++ config.fish
@@ -13,8 +13,22 @@ bind \ct transpose-chars
 source ~/.profile
 source ~/.fish_abbrs.fish
 
+
 function postexec_source_profile --on-event fish_postexec
-  if test "$argv" = "vi ~/.profile"
-    source ~/.profile
-  end
+    if test "$argv" = "vi ~/.profile"
+        echo -n 'Sourcing .profile... '
+        source ~/.profile
+        echo Done.
+    end
+
+    if test "$argv" = "vi /etc/hosts"
+        echo -n 'Flushing dns... '
+        dscacheutil -flushcache
+        echo Done.
+    end
+end
+
+
+function fish_vterm_prompt_end --on-event fish_prompt
+    printf '\e]51;A'(whoami)'@'(hostname)':'(pwd)'\e\\'
 end
---
+++ functions/sync-repos.fish
@@ -0,0 +1,8 @@
+function sync-repos
+	for repo in ~/.config/fish ~/.spacemacs.d ~/.dotfiles ~/.password-store/
+echo $repo
+git -C $repo stat
+git -C $repo pull
+git -C $repo push
+end
+end
