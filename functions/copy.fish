3d141a57ee5b758ea7e0a45d07268955bcd85095
Razzi Abuissa <razzi53@gmail.com>
2020-02-14 04:35:37 -0800

    Fix multiple condition `copy` and switch from [ to test

--- functions/copy.fish
+++ functions/copy.fish
@@ -1,8 +1,9 @@
+# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.3wOdoI/copy.fish @ line 1
 function copy
     set count (count $argv | tr -d \n)
-    if [ $count = 2 ] and [ -d $argv[1] ]
+    if test "$count" = 2; and test -d "$argv[1]"
         command cp -r $argv
-    else if [ $count = 1 ]
+    else if test "$count" = 1
         cp (echo $argv | trim-right /) .
     else
         command cp $argv
