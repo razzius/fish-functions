ccf5bca04c57e53ee58f01e459b194691d3c4a3c
Razzi Abuissa <razzi53@gmail.com>
2020-03-11 17:12:57 -0700

    Sort songs before playing

--- functions/song.fish
+++ functions/song.fish
@@ -1,5 +1,6 @@
+# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.Tmvw2X/song.fish @ line 2
 function song
-    fd --full-path (echo "$argv" | string replace --all ' ' '.*')".*.(m4a|mp3|flac|wav)" ~/Music/ | read -z songs
+    fd --full-path (echo "$argv" | string replace --all ' ' '.*')".*.(m4a|mp3|flac|wav)" ~/Music/ | sort | read -z songs
     set n_songs (echo -n "$songs" | wc -l | coln 1)
     if test $n_songs -gt 1
         echo more than 1
