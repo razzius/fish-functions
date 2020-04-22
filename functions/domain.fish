0f56d51e44d5d3c545b480e67e9731385fea2ba1
Razzi Abuissa <razzi53@gmail.com>
2020-04-22 15:26:53 -0700

    Update functions/domain.fish

--- functions/domain.fish
+++ functions/domain.fish
@@ -1,3 +1,5 @@
-function domain --argument url
+# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.twuUHC/domain.fish @ line 2
+function domain
+    read url
     echo $url | cut -d '/' -f 3
 end
