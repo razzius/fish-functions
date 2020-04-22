05227141db999e015d4ad7892efc81a6143baaab
Razzi Abuissa <razzi53@gmail.com>
2020-04-22 15:28:25 -0700

    Enable getting pw domain from pasting

--- functions/pw.fish
+++ functions/pw.fish
@@ -1,5 +1,17 @@
-function pw --argument url --wraps pass
-    set domain (domain $url)
+function pw --wraps=pass --argument url
+    if string-empty $url
+        set clipboard (pbpaste)
+
+        if not echo $clipboard | string match -rq '^https?://'
+            echo 'pw: clipboard does not contain http url'
+            return 1
+        end
+
+        set domain (pbpaste | domain)
+    else
+        set domain (echo $url | domain)
+    end
+
     if pass list $domain >/dev/null
         pass -c $domain
     else
