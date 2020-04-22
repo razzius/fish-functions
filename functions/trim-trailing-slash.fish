7daf23e6251140b4689a7772dd08e9ca875ba1a1
Razzi Abuissa <razzi53@gmail.com>
2020-04-22 15:29:58 -0700

    indentation

--- functions/trim-trailing-slash.fish
+++ functions/trim-trailing-slash.fish
@@ -1,5 +1,5 @@
 # Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.Qo3rSu/trim-trailing-slash.fish @ line 2
 function trim-trailing-slash
-  read str
-  string replace -r '/$' '' $str
+    read str
+    string replace -r '/$' '' $str
 end
