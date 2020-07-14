aab8a6b8471cdea142c51848a774fd8f4638c617
Razzi Abuissa <razzi53@gmail.com>
2020-07-14 00:57:25 -0500

    Make last-function-name only find commands starting with function or funced

--- functions/last-function-name.fish
+++ functions/last-function-name.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.Eexn3m/last-function-name.fish @ line 1
 function last-function-name --argument index
-    history --prefix func | string replace -- ' -s ' ' ' | awk 'NF>1' | row 1 | coln 2
+    history --prefix func | grep -w -E '^(function|funced)' | string replace -- ' -s ' ' ' | awk 'NF>1' | row 1 | coln 2
 end
