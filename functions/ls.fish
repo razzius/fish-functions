ebfda85df4ae541ed90827e87dede44149a36aa4
Razzi Abuissa <razzi@abuissa.net>
2021-08-15 02:47:46 -0700

    rm ls alias and and fix search

--- functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --description 'List contents of directory'
-    gls --color $argv
-end
--- functions/search.fish
+++ functions/search.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/7v/b0z6dsqj1fx8k5rf8x121l580000gn/T//fish.tZSRED/search.fish @ line 2
 function search
-    open https://duckduckgo.com/"$argv"
+    open https://duckduckgo.com/(echo $argv | string replace --all ' ' '%20')
 end
