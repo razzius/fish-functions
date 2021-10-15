164b6c729e4a01e24937ffa36aa0522f38e2b42c
Razzi Abuissa <razzi@abuissa.net>
2021-10-15 12:16:31 -0400

    Fix bug with functions/clone-cd.fish

--- functions/clone-cd.fish
+++ functions/clone-cd.fish
@@ -1,13 +1,13 @@
 function clone-cd --argument url _destination
-    set destination (default $_destination (repo-from-url $repo))
+    set destination (default $_destination (repo-from-url $url))
     if file-exists $destination
         cd $destination && git pull
         return
     end
 
-    if test (ls $destination | word-count) != 0
+    if test (ls $destination &> /dev/null | word-count) != 0
         mkdir $destination
     end
 
-    git clone --depth=1 $repo $destination && cd $destination
+    git clone --depth=1 $url $destination && cd $destination
 end
