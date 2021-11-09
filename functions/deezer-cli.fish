73713b1fa2fc9f1106e3fcb33911e51c79a5bb19
Razzi Abuissa <razzi@abuissa.net>
2021-11-09 02:23:14 -0600

    Add functions/deezer-cli.fish

---
+++ functions/deezer-cli.fish
@@ -0,0 +1,4 @@
+function deezer-cli
+# osascript -e 'tell application "Deezer" to activate'; cliclick c:80,120 c:300,50 t:"$argv" kp:enter w:1500 c:280,350
+osascript -e 'tell application "Deezer" to activate'; cliclick c:80,120 c:300,50 kp:v kp:enter w:1500 c:280,350
+end
