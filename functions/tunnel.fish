73a5975b317754a3f9cfe0d136f42016fe87bff1
Razzi Abuissa <razzi@abuissa.net>
2021-11-09 00:03:38 -0600

    Update functions/tunnel.fish

--- functions/tunnel.fish
+++ functions/tunnel.fish
@@ -3,7 +3,9 @@ function tunnel --argument host port
     echo "Opening http://localhost:$port in browser..."
     ssh -NL $port:$host:$port $host &
     set ssh_pid $last_pid
-    sh -c "sleep 2; open http://localhost:$port" &
+    # todo https://stackoverflow.com/questions/29954169/automatically-establish-ssh-tunnel-wait-until-ssh-tunnel-is-established-then-e
+    # rather than sleep 3
+    sh -c "sleep 3; open http://localhost:$port" &
     # tada a hack; I want the prompt to be on its own line
     echo "Hit ENTER to stop."
     read -P ""
