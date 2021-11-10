c08871334175c6f46c681ee4e60f8c09922d2783
Razzi Abuissa <razzi@abuissa.net>
2021-11-09 21:31:31 -0600

    Update functions/deezer-cli.fish

--- functions/deezer-cli.fish
+++ functions/deezer-cli.fish
@@ -1,4 +1,10 @@
 function deezer-cli
-# osascript -e 'tell application "Deezer" to activate'; cliclick c:80,120 c:300,50 t:"$argv" kp:enter w:1500 c:280,350
-osascript -e 'tell application "Deezer" to activate'; cliclick c:80,120 c:300,50 kp:v kp:enter w:1500 c:280,350
+  pbpaste | read _clipboard
+  echo $argv | pbcopy
+  osascript -e 'tell application "Deezer" to activate'
+  cliclick c:80,120 c:300,50
+  sleep .1
+  cliclick kd:cmd t:v ku:cmd
+  cliclick kp:enter w:1500 c:280,350
+  echo $_clipboard | pbcopy
 end
