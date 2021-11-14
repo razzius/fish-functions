edb7f53157314b04c842210108659a93d0650ee7
Razzi Abuissa <razzi@abuissa.net>
2021-11-14 13:58:38 -0600

    Better deezer cli 😔

--- functions/deezer-cli.fish
+++ functions/deezer-cli.fish
@@ -2,9 +2,13 @@ function deezer-cli
   pbpaste | read _clipboard
   echo $argv | pbcopy
   osascript -e 'tell application "Deezer" to activate'
-  cliclick c:80,120 c:300,50
-  sleep .1
-  cliclick kd:cmd t:v ku:cmd
-  cliclick kp:enter w:1500 c:280,350
+  cliclick -r \
+    kd:cmd kp:arrow-left ku:cmd \
+    c:80,120 c:300,50 w:100 \
+    kd:cmd t:a ku:cmd \
+    kd:cmd t:v ku:cmd \
+    kp:enter \
+    w:1500 \
+    c:280,350
   echo $_clipboard | pbcopy
 end
