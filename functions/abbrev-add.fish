71c22c9616542777aa96c5e8560e3195c5176642
Razzi Abuissa <razzi@abuissa.net>
2021-11-18 16:37:51 -0600

    1-argument for of abbrev-add to add abbr for last command

--- functions/abbrev-add.fish
+++ functions/abbrev-add.fish
@@ -1,3 +1,9 @@
 function abbrev-add --argument shorthand
-    abbr -a -- $shorthand (echo $argv[2..] | unexpand-home-tilde)
+  if test (count $argv) -eq 1
+    set last_cmd (history | head -1)
+    abbr -a -- $shorthand $last_cmd
+    return
+  end
+
+  abbr -a -- $shorthand (echo $argv[2..] | unexpand-home-tilde)
 end
