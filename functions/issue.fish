c617890c47ae46ed0215d9a9ac6b9556be9652b7
Razzi Abuissa <razzi@abuissa.net>
2021-11-20 03:18:41 -0600

    improvements to `new` and `issue`

--- functions/issue.fish
+++ functions/issue.fish
@@ -4,23 +4,21 @@ function issue
   if not string-empty $_flag_directory
     if is-dir $_flag_directory && is-dir $_flag_directory/.git
       set repo_dir $_flag_directory
+      set url (git -C $repo_dir remote get-url --push origin)
+      set repo_info (echo $url | trim-left git@github.com: | trim-right .git)
+      set repo_flags "-R" "$repo_info"
     else
       echo issue: $_flag_directory is not a git repository
       return 1
     end
-  else
-    set repo_dir .
   end
-
-  set -l url (git -C $repo_dir remote get-url --push origin)
-  set -l repo_info (echo $url | trim-left git@github.com: | trim-right .git)
 
   set label_args ''
   for label in $argv
-    gh label -R $repo_info create $label
+    gh label $repo_flags create $label
     set label_args $label_args --label $label
   end
 
   set title (read --prompt-str "Title: ")
-  eval "gh issue create -R $repo_info --title "$title" $label_args"
+  gh issue $repo_flags create --title "$title" (echo "$label_args" | string trim | string split ' ')
 end
--- functions/new.fish
+++ functions/new.fish
@@ -1,7 +1,10 @@
 function new --argument project
-    mkdir $project
-    cd $project
-    git init
-    gh repo create $project
-    git branch --set-upstream-to=origin/main main
+    mkdir $project \
+      && cd $project \
+      && git init \
+      && gh repo create --public --confirm $project \
+      && echo "# $project" > README.md \
+      && git add README.md \
+      && git commit -m 'Initial commit' \
+      && git push --set-upstream
 end
