7e8b1c3e92e319b7ce26e8ec73b5f032be2e6502
Razzi Abuissa <razzi@abuissa.net>
2021-11-20 03:55:17 -0600

    Swimplify issue.fish using cd...

--- functions/issue.fish
+++ functions/issue.fish
@@ -1,12 +1,10 @@
 function issue
+  set _orig_dir $PWD
   argparse d/directory= -- $argv
 
   if not string-empty $_flag_directory
     if is-dir $_flag_directory && is-dir $_flag_directory/.git
-      set repo_dir $_flag_directory
-      set url (git -C $repo_dir remote get-url --push origin)
-      set repo_info (echo $url | trim-left git@github.com: | trim-right .git)
-      set repo_flags "-R" "$repo_info"
+      cd $_flag_directory
     else
       echo issue: $_flag_directory is not a git repository
       return 1
@@ -15,10 +13,11 @@ function issue
 
   set label_args ''
   for label in $argv
-    gh label $repo_flags create $label
+    gh label create $label 2> /dev/null
     set label_args $label_args --label $label
   end
 
   set title (read --prompt-str "Title: ")
-  gh issue $repo_flags create --title "$title" (echo "$label_args" | string trim | string split ' ')
+  gh issue create --title "$title" (echo "$label_args" | string trim | string split ' ')
+  cd $_orig_dir
 end
