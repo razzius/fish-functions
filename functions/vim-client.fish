9343e8242a56bacb6a60408796593bb4d249a56c
Razzi Abuissa <razzi@abuissa.net>
2023-07-19 01:16:09 -0500

    fish_indent vim-client.fish

--- functions/vim-client.fish
+++ functions/vim-client.fish
@@ -1,8 +1,8 @@
 function vim-client --argument file
-  if string-empty $VIM_TERMINAL
-    echo not in vim terminal
-    return 1
-  end
+    if string-empty $VIM_TERMINAL
+        echo not in vim terminal
+        return 1
+    end
 
-  echo -e "\033]51;[\"drop\", \"$file\"]\007"
+    echo -e "\033]51;[\"drop\", \"$file\"]\007"
 end
