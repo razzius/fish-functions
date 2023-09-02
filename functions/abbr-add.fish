5b81984caaf9bc6051d160b0d298944fa2414bc2
Razzi Abuissa <razzi@abuissa.net>
2023-09-01 21:46:02 -0500

    abbr-add: remove matching abbrs before adding a new one

--- functions/abbr-add.fish
+++ functions/abbr-add.fish
@@ -1,8 +1,16 @@
 function abbr-add --argument shorthand
     set expansion (echo $argv[2..] | unexpand-home-tilde | trim-trailing-slash)
-    set abbr_command "abbr -a -- $shorthand \"$expansion\""
+    set base_abbr "abbr -a -- $shorthand"
+    set abbr_command "$base_abbr \"$expansion\""
     eval "$abbr_command"
     set abbr_file "$HOME/.config/fish/conf.d/abbrs.fish"
-    echo "$abbr_command" >>$abbr_file
+
+    set abbr_match (grep $base_abbr $abbr_file)
+    if not string-empty $abbr_match
+      echo Removing conflicting abbr $abbr_match
+      sed -i /$base_abbr/d $abbr_file
+    end
+
+    echo "$abbr_command" >> $abbr_file
     sort -o $abbr_file $abbr_file
 end
