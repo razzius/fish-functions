8addcddb4eef2d2ae6020b85b4dcb432c8359ea6
Razzi Abuissa <razzi@abuissa.net>
2023-09-27 01:25:23 -0500

    Update test/test_readme.py

--- test/test_readme.py
+++ test/test_readme.py
@@ -1,4 +1,20 @@
+import os
+
 with open('README.md') as f:
     readme = f.read()
 
-print(readme)
+function_lines = [
+    line for line in readme.splitlines()
+    if line.startswith('###')
+]
+
+functions = [line.split()[1].strip('`') for line in function_lines]
+
+missing_functions = [
+    function
+    for function in functions
+    if not os.path.exists(f'functions/{function}.fish')
+]
+
+if missing_functions:
+    print(f'Missing functions: {missing_functions}')
