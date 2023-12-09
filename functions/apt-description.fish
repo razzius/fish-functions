b5b3eea19376f09657f8d6256eff06ee97e07989
Razzi Abuissa <razzi@abuissa.net>
2023-12-09 14:05:52 -0600

    Make apt-description find Description as well as Description-en

--- functions/apt-description.fish
+++ functions/apt-description.fish
@@ -1,3 +1,3 @@
 function apt-description --argument package
-apt-cache show $package | sed '/^$/Q' | grep --color=never -E '^Description-en:|^ '
+apt-cache show $package | sed '/^$/Q' | grep --color=never -E '^Description(-en)?:|^ '
 end
