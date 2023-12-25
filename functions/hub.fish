a729d52bb6fa1465532c1888cbbdcd704ad923a0
Razzi Abuissa <razzi@abuissa.net>
2023-12-25 02:21:12 -0600

    Make hub work with already-https urls

--- functions/hub.fish
+++ functions/hub.fish
@@ -1,5 +1,5 @@
 function hub
     set url (git config --get remote.origin.url)
-    set https_url (echo $url | string replace ':' '/' | string replace 'git@' 'https://')
+    set https_url (echo $url | string replace -r 'git@(.+):' 'https://$1/')
     $BROWSER $https_url
 end
