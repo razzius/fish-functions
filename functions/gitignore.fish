a0e28439df5402f936502ba687fb8bdc2bfd956a
Razzi Abuissa <razzi@abuissa.net>
2023-12-28 05:47:06 -0600

    Allow gitignore to ignore multiple patterns in 1 command

--- functions/gitignore.fish
+++ functions/gitignore.fish
@@ -1,5 +1,7 @@
-function gitignore --argument pattern
+function gitignore
     set gitignore_file (git root)/.gitignore
     ensure-trailing-newline $gitignore_file
-    echo $pattern >> $gitignore_file
+    for pattern in $argv
+        echo $pattern >> $gitignore_file
+    end
 end
