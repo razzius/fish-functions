85e6e5286862c7d4d6baf155aa34f712ae7dd51c
Razzi Abuissa <razzi@abuissa.net>
2024-01-17 14:29:08 -0600

    Update test_symlink to not cd .. unnecessarily at end of test

--- test/test_symlink.fish
+++ test/test_symlink.fish
@@ -4,8 +4,6 @@ mkdir-cd test_symlink
 
 symlink nonexistant anything 2> error.txt
 
-echo error.txt should have: "symlink: `from` argument does not exist"
+echo error.txt should have: "symlink: `from` argument `...` does not exist"
 echo here it is:
 cat error.txt
-
-cd ..
