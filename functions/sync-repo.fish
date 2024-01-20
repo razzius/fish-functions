f46024d58f94a17fd3cda3be32503bf00a51187a
Razzi Abuissa <razzi@abuissa.net>
2024-01-20 01:08:10 -0600

    Set repo for sync-repo pre-push test

--- functions/sync-repo.fish
+++ functions/sync-repo.fish
@@ -1,7 +1,7 @@
 function sync-repo --argument repo
     git -C "$repo" status -sb
     git -C "$repo" pull
-    if test (git rev-list --count @{upstream}..HEAD) -gt 0
+    if test (git -C "$repo" rev-list --count @{upstream}..HEAD) -gt 0
         git -C "$repo" push
     end
 end
