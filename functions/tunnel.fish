e6051dcc32e6c247baa3e4b6b4b5a61b18e6f36a
Razzi Abuissa <razzi@abuissa.net>
2024-01-23 15:10:56 -0600

    Add comment to tunnel about ssh still running

--- functions/tunnel.fish
+++ functions/tunnel.fish
@@ -1,9 +1,13 @@
 function tunnel --argument host port
     echo "> ssh -NL $port:$host:$port $host"
+
     ssh -NL $port:$host:$port $host &
     set ssh_pid $last_pid
     sleep .5
-    kill -0 $ssh_pid 2>/dev/null || return
+
+    # Check if ssh is still running; if not, exit
+    kill -0 $ssh_pid 2> /dev/null || return 1
+
     retry sh -c "nc -z localhost $port 2> /dev/null"
     echo "Opening http://localhost:$port in browser..."
     open http://localhost:$port
