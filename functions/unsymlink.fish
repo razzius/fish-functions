cd23155a077b0385847a8378aa62f5451e718d52
Razzi Abuissa <razzi@abuissa.net>
2024-01-27 00:02:24 -0600

    Quote argument to echo in unsymlink

--- functions/unsymlink.fish
+++ functions/unsymlink.fish
@@ -1,5 +1,5 @@
 function unsymlink --argument _file
     set file (echo $_file | trim-trailing-slash)
     is-symlink $file and rm $file
-    or echo unsymlink: not a symlink: $file
+    or echo "unsymlink: not a symlink: $file"
 end
