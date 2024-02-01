775f9b1265efcbe05f7736cdbdea0543e77a4f8e
Razzi Abuissa <razzi@abuissa.net>
2024-02-01 00:56:45 -0600

    Remove some unused macos-specific functions

--- functions/start-elastic.fish
+++
@@ -1,3 +0,0 @@
-function start-elastic
-    docker run -d -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
-end
--- functions/start-jupyter.fish
+++
@@ -1,4 +0,0 @@
-function start-jupyter
-    launchctl start local.jupyter.notebook
-    echo "rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 8888" | sudo pfctl -ef - ^/dev/null
-end
