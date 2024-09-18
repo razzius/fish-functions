fa715211bd21ec1bb05f24f3252186b411d400cb
Razzi Abuissa <razzi53@gmail.com>
2024-09-17 23:03:37 -0500

    Make is-symlink error if more than 1 argument is passed

--- functions/is-symlink.fish
+++ functions/is-symlink.fish
@@ -1,3 +1,7 @@
-function is-symlink --argument file
+function is-symlink --argument file --argument extra
+    if not string-empty $extra
+        echo 'is-symlink: too many arguments' >&2
+        return 1
+    end
     test -L (echo $file | trim-trailing-slash)
 end
