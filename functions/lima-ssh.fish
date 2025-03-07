6baa6ac853351f1ba323f6742fc7d1fbc74b7081
Razzi Abuissa <razzi@abuissa.net>
2025-03-07 01:23:11 -0600

    Make lima-ssh check status without stderr output

--- functions/lima-ssh.fish
+++ functions/lima-ssh.fish
@@ -1,6 +1,6 @@
 function lima-ssh
-    set default_status (limactl list default -f '{{.Status}}')
-    if test $status != 0 || test $default_status = Stopped
+    set default_status (limactl list default -f '{{.Status}}' 2> /dev/null)
+    if string-empty $default_status || equals $default_status Stopped
         limactl start --tty=false
     end
     lima
