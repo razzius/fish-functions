52a0465cd0d115235347df428298e5e137f5cfc2
Razzi Abuissa <razzi@abuissa.net>
2025-03-07 02:06:56 -0600

    Make abbr-add not use abbr_file unnecessary temporary variable

--- functions/abbr-add.fish
+++ functions/abbr-add.fish
@@ -2,6 +2,5 @@ function abbr-add
     set cleaned_args (echo $argv | unexpand-home-tilde | trim-trailing-slash)
     abbr -a (echo $cleaned_args | string split ' ')
 
-    set abbr_file "$HOME/.config/fish/conf.d/abbrs.fish"
-    abbr > $abbr_file
+    abbr > "$HOME/.config/fish/conf.d/abbrs.fish"
 end
