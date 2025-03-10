434a4b52ccc28351bb30d9d9a26f08467c3069aa
Razzi Abuissa <razzi@abuissa.net>
2025-03-10 13:34:26 -0500

    Add cleanup to test_symlink.fish
    It would be even better to have the test have an exit status depending
    on the behavior of the function being tested.

--- test/test_symlink.fish
+++ test/test_symlink.fish
@@ -7,3 +7,7 @@ symlink nonexistant anything 2> error.txt
 echo error.txt should have: "symlink: `from` argument `...` does not exist"
 echo here it is:
 cat error.txt
+
+# clean up. TODO have a environment variable to avoid cleaning up for inspection
+rm error.txt
+rmdir-.
