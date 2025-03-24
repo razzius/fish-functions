f6257bd197f7b1999994c9fb8d99c15a1a65a053
Razzi Abuissa <razzi@abuissa.net>
2025-03-24 01:55:28 -0500

    Start of unsymlink upgrades
    completion: find both directories and symlinks to allow unsymlinking files in other directories.
    
    README: add `us` abbr for unsymlink
    
    unsymlink: better error handling, using exit status 1
    
    Still doesn't work with ~ paths or ../ paths.

--- completions/unsymlink.fish
+++ completions/unsymlink.fish
@@ -1,2 +1,23 @@
 complete -c unsymlink -f
-complete -c unsymlink -a '(find . -maxdepth 1 -type l)'
+complete -c unsymlink -a '(__unsymlink_complete_args)'
+
+function __unsymlink_complete_args
+    set -l previous_token (commandline -x)[-1]
+    if equals $previous_token unsymlink
+        set base_path ''
+    else
+        set base_path $previous_token
+    end
+
+    if equals $base_path ''
+        set find_path .
+    else
+        set find_path $base_path
+    end
+
+    # Make directories end with / to keep exploring them
+    find $find_path -mindepth 1 -maxdepth 1 -type d -printf "$base_path%f/\n"
+
+    # Symlinks end in just the symlink name
+    find $find_path -mindepth 1 -maxdepth 1 -type l -printf "$base_path%f\n"
+end
