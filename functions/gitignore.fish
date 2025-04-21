39e0d9b50865f93db1d3ec194c1cb23a9cc738b3
Razzi Abuissa <razzi@abuissa.net>
2025-04-21 01:46:00 -0500

    Make gitignore error out if not in git repository

--- functions/gitignore.fish
+++ functions/gitignore.fish
@@ -1,5 +1,5 @@
 function gitignore
-    set gitignore_file (git root)/.gitignore
+    set gitignore_file (git root)/.gitignore || return 1
     ensure-trailing-newline $gitignore_file
     for pattern in $argv
         echo $pattern >> $gitignore_file
