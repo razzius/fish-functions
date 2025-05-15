74c65daf2a4b15ea8f0393b670bb43e577447cf3
Razzi Abuissa <razzi@abuissa.net>
2025-05-15 01:05:30 -0500

    Update implementation of symlink to make it work better with nonexistant target directories. Old test still passes

--- functions/symlink.fish
+++ functions/symlink.fish
@@ -1,10 +1,10 @@
-function symlink --argument _from _to
+function symlink --argument-names _from --argument-names _to
     if string-empty $_from; or string-empty $_to
         echo "symlink: must provide from and to arguments"
         return 1
     end
-
-    set abs_to (realpath "$_to") || return 1
+    set target (basename $_to)
+    set abs_to (realpath (dirname "$_to"))/$target || return 1
 
     if test -d $_to && not test -d $_from
         set to "$abs_to/"(basename $_from)
@@ -12,10 +12,10 @@ function symlink --argument _from _to
         set to "$abs_to"
     end
 
-    set from (realpath "$_from") || return 1
+    set from (realpath $_from 2> /dev/null)
 
-    if not test -e $from
-        echo "symlink: `from` argument '$from' does not exist" >&2
+    if string-empty $from || not test -e $from
+        echo "symlink: `from` argument '$_from' does not exist" >&2
         return 1
     end
 
