f8e2dc7e07900daf436b0ad40814dece93ab32c1
Razzi Abuissa <razzi@abuissa.net>
2025-07-22 01:12:35 -0500

    Make sync-repos use literal ~ in its error message

--- functions/sync-repos.fish
+++ functions/sync-repos.fish
@@ -1,17 +1,17 @@
 function sync-repos
     if not file-exists ~/.repos.txt
-        echo Put the repositories to sync in ~/.repos.txt
+        echo 'Put the repositories to sync in ~/.repos.txt'
         return 1
     end
-
+    
     for path in (cat ~/.repos.txt | grep -v '^#')
         set repo (echo $path | expand-home-tilde)
-
+        
         if not is-dir $repo
             echo $repo not present
             continue
         end
-
+        
         echo $path
         if not sync-repo $repo
             echo 'sync-repos: canceling'
