d10cc96126b7d5f5ec0d5dac61658fa408514923
Razzi Abuissa <razzi@abuissa.net>
2025-10-25 21:31:55 -0500

    Remove mkdir from copy - I don't think it was doing anything good...

--- functions/copy.fish
+++ functions/copy.fish
@@ -1,9 +1,9 @@
 function copy
     set count (count $argv | tr -d \n)
+
     if test "$count" = 2; and test -d "$argv[1]"
         set from (echo $argv[1] | trim-right /)
-        set to (echo $argv[2])
-        mkdir -p (basename $to)
+        set to $argv[2]
         command cp -i -r $from $to
     else
         command cp -i $argv
