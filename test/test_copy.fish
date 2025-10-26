ff629af7532b011cf0f74faa9adad1e915c987b4
Razzi Abuissa <razzi@abuissa.net>
2025-10-25 21:24:53 -0500

    Start to add test for copy to document spurious directory creation (#27)

--- test/test_copy.fish
+++ test/test_copy.fish
@@ -12,25 +12,46 @@ function __fail
     return 1
 end
 
-set contents 'contents a'
+function test_basic_behavior
+    set contents 'contents a'
 
-echo $contents > a.txt
+    echo $contents > a.txt
 
-copy a.txt b.txt 2> error.txt
+    copy a.txt b.txt 2> error.txt
 
-set result_status $status
-set expected_status 0
+    set result_status $status
+    set expected_status 0
 
-if not equals $result_status $expected_status
-    echo Expected status $expected_status but got $result_status
-    __fail
+    if not equals $result_status $expected_status
+        echo Expected status $expected_status but got $result_status
+        __fail
+    end
+
+    set output (cat b.txt)
+
+    if not string match -q $contents $output
+        echo `copy` did not behave as expected.
+        __fail
+    end
+
 end
 
-set output (cat b.txt)
+function test_to_multiple_directory_levels
+    mkdir from_dir
 
-if not string match -q $contents $output
-    echo `copy` did not behave as expected.
-    __fail
+    echo 'contents' > from_dir/file.txt
+
+    mkdir -p dir1/dir2
+
+    copy from_dir/ dir1/dir2/
+
+    breakpoint
 end
 
+function main
+    test_basic_behavior
+    test_to_multiple_directory_levels
+end
+
+main
 __cleanup
