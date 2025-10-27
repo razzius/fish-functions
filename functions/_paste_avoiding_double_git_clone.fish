59c15ec85600c1e5398acb6da0ed2dd007e44e6e
Razzi Abuissa <razzi@abuissa.net>
2025-10-27 10:44:37 -0500

    Fix function name _paste_avoiding_double_git_clone.fish to actually match its filename

--- functions/_paste_avoiding_double_git_clone.fish
+++ functions/_paste_avoiding_double_git_clone.fish
@@ -1,4 +1,4 @@
-function paste_avoiding_double_git_clone
+function _paste_avoiding_double_git_clone
     set command (commandline | string trim)
     set clipboard (fish_clipboard_paste | string collect -N)
     if string match -qr '(clone-cd|git clone)' "$command"
