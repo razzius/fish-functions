b62c4c87f6774efb16c63e4956c415eb97097c43
Razzi Abuissa <razzi@abuissa.net>
2025-10-28 00:48:04 -0500

    Change wifi-network-name to use networksetup so it doesn't just come back as <redacted>

--- functions/wifi-network-name.fish
+++ functions/wifi-network-name.fish
@@ -1,4 +1,4 @@
 function wifi-network-name
     set device (networksetup -listallhardwareports | grep 'Wi-Fi' -A 1 | tail -1 | coln 2)
-    ipconfig getsummary $device | grep '  SSID :' | coln 3
+    networksetup -listpreferredwirelessnetworks $device | row 2 | string trim
 end
