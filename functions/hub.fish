321512748ddfb6b3cc91c57c58435f1190a29d5e
Razzi Abuissa <razzi@abuissa.net>
2025-10-28 23:00:09 -0500

    Allow opening other remotes than origin with `hub`

--- functions/hub.fish
+++ functions/hub.fish
@@ -1,5 +1,7 @@
-function hub
-    set url (git config --get remote.origin.url)
+function hub --argument _remote
+    set remote (default $_remote origin)
+
+    set url (git config --get remote.$remote.url)
     set https_url (echo $url | string replace -r 'git@(.+):' 'https://$1/')
     $BROWSER $https_url
 end
