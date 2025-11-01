37732548e4634e01c90775c0d616fea29b38d0e3
Razzi Abuissa <razzi@abuissa.net>
2025-11-01 14:04:15 -0500

    Update functions/new.fish

--- functions/new.fish
+++ functions/new.fish
@@ -1,8 +1,7 @@
 function new --argument project
-    mkdir $project \
-        && cd $project \
+    mkdir-cd $project \
         && git init \
-        && echo "# $project" >README.md \
+        && echo "# $project" > README.md \
         && git add README.md \
         && git commit -m 'Initial commit'
 end
