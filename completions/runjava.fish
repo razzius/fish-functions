eefd1f66340b393168401a73926af5d06578e947
Razzi Abuissa <razzi@abuissa.net>
2025-11-15 08:07:03 -0600

    Fix runjava and add completion

---
+++ completions/runjava.fish
@@ -0,0 +1,1 @@
+complete -x -c runjava -a '(find . -maxdepth 1 -name "*.java" -exec basename {} \;)'
--- functions/runjava.fish
+++ functions/runjava.fish
@@ -1,4 +1,4 @@
 function runjava --argument-names file
     javac $file
-    java (string replace .java '' $file) $argv[2..0]
+    java (string replace .java '' $file) $argv[2..1]
 end
