892e8fbb4cf0572fe2ba67fa34582037ff94ec29
Razzi Abuissa <razzi@abuissa.net>
2025-11-15 23:53:10 -0600

    Rename functions/_paste_avoiding_double_git_clone.fish to functions/_paste-avoiding-double-git-clone.fish

--- functions/_paste-avoiding-double-git-clone.fish
+++ functions/_paste-avoiding-double-git-clone.fish
@@ -1,7 +1,7 @@
-function _paste_avoiding_double_git_clone
+function _paste-avoiding-double-git-clone
     set command (commandline | string trim)
     set clipboard (fish_clipboard_paste | string collect -N)
-    if string match -qr '(clone-cd|git clone)' "$command"
+    if string match -qr '(clone-cd|clone-shallow-cd|git clone)' "$command"
         set clipboard (echo $clipboard | string replace 'git clone ' '')
     end
     commandline -i -- $clipboard
