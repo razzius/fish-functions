c08db0e2751a01005c4bc2ab879583c6f5b944ee
Razzi Abuissa <razzi@abuissa.net>
2025-12-07 22:19:20 -0600

    Fix wifi-network-name which in turn fixes wifi-password

--- functions/wifi-network-name.fish
+++ functions/wifi-network-name.fish
@@ -1,4 +1,4 @@
 function wifi-network-name
     set device (networksetup -listallhardwareports | grep 'Wi-Fi' -A 1 | tail -1 | coln 2)
-    networksetup -listpreferredwirelessnetworks $device | row 2 | string trim
+    networksetup -getairportnetwork $device | awk -F ': ' '{print $2}'
 end
