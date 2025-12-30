ca48910af19d241ae2cc090de2b3e04044bcc695
Razzi Abuissa <razzi@abuissa.net>
2025-12-30 16:09:27 -0600

    Add failing test of is-clean-zip with mixed folder and file in toplevel

--- test/test_is_clean_zip.fish
+++ test/test_is_clean_zip.fish
@@ -4,6 +4,7 @@ function __cleanup
     if string-empty $DEBUG
         remove -rf clean_dir clean_dir.zip
         remove -rf file.txt dirty_dir dirty_dir.zip
+        remove -rf folder mixed_dir.zip
         rmdir-.
     end
 end
@@ -33,9 +34,31 @@ function test_is_clean_zip_not_clean
     end
 end
 
+function test_is_clean_zip_mix_folder_files
+    touch file.txt
+
+    mkdir folder
+    touch folder/clean_file.txt
+
+    zip --quiet -r mixed_dir.zip file.txt folder
+
+    if is-clean-zip mixed_dir.zip
+        error is-clean-zip of a zip that has a directory but also toplevel files should be dirty
+    end
+
+    rm mixed_dir.zip
+
+    zip --quiet -r mixed_dir.zip folder file.txt
+
+    if is-clean-zip mixed_dir.zip
+        error "is-clean-zip of a zip that has a directory but also toplevel files should be dirty (other order)"
+    end
+end
+
 function main
     test_is_clean_zip_true_positive
     test_is_clean_zip_not_clean
+    test_is_clean_zip_mix_folder_files
 end
 
 main
