22e28e685b9e8e29aa8cef87aa6ebb4b47c0533f
Razzi Abuissa <razzi@abuissa.net>
2025-12-31 19:59:37 -0600

    More tests/functionality for move, overwriting multiple files in a directory
    Complicated the logic a bit, but it now handles multiple files being
    moved into a directory that has contents committed in git.
    
    Closes #32.

--- functions/move.fish
+++ functions/move.fish
@@ -1,18 +1,34 @@
-function move
-    set from $argv[1]
-
+function _move_single --argument-names from to
     if is-symlink $from; and string match --quiet --regex --entire '/$' $from
         error 'move: `from` argument "'$from'" is a symlink with a trailing slash.'
         error 'move: to rename a symlink, remove the trailing slash from the argument.'
         return 1
     end
 
-    set destination_files $argv[2]
+    if file-committed-in-git $to
+        mv $from $to
+    else
+        mv -i $from $to
+    end
+end
 
-    if file-committed-in-git $destination_files
-        mv $argv
+function move
+    set froms $argv[1..-2]
+
+    if equals (count $argv) 2
+        _move_single $argv
         return $status
     end
 
-    mv -i $argv
+    set target $argv[-1]
+    if not is-dir $target
+        error "move: $target is not a directory"
+        return 1
+    end
+
+    for f in $froms
+        set to $target$f
+
+        _move_single $f $to
+    end
 end
--- test/test_move.fish
+++ test/test_move.fish
@@ -53,15 +53,57 @@ function test_move_no_confirm_committed_file
     cd ..
 end
 
-function test_move_no_confirm_multiple_committed_files
-    error 'todo'
-    return 1
+function test_move_confirm_mixed_committed_files
+    mkdir-cd repo
+    mkdir target
+    echo 'a' > target/a.txt
+    echo 'b' > target/b.txt
+    git init --quiet
+    git add target/a.txt
+    git commit --quiet -m 'Add target/a.txt which would be overwritten'
+    echo 'new_a' > a.txt
+    echo 'new_b' > b.txt
+    echo n | move a.txt b.txt target/ 2> move_stdout.txt
+
+    # target/b.txt should not be overwritten
+    if not equals (cat target/b.txt) 'b'
+        error 'move should not overwrite target/b.txt which was not tracked in git'
+        return 1
+    end
+
+    if not equals (cat target/a.txt) 'new_a'
+        error 'move should have overwritten target/a.txt since it was committed in git'
+        return 1
+    end
+
+    if not equals (ls target | line-count) 2
+        error 'there should only be 2 files in target/'
+        return 1
+    end
+    cd ..
+end
+
+function test_move_error_multiple_file_not_dir
+    touch a b c
+    move a b c 2> move_stderr.txt
+    set result_status $status
+
+    if not equals $result_status 1
+        error "move should have exited with status 1 but got $result_status"
+        return 1
+    end
+
+    if not equals (cat move_stderr.txt) "move: c is not a directory"
+        error "move stderr unexpected: "(cat move_stderr.txt)
+        return 1
+    end
 end
 
 function main
     test_move_symlink_error
     and test_move_no_confirm_committed_file
-    and test_move_no_confirm_multiple_committed_files
+    and test_move_confirm_mixed_committed_files
+    and test_move_error_multiple_file_not_dir
 end
 
 main
