2a47812b6a1e8a5b56a47d553dbe0505592180c6
Razzi Abuissa <razzi@abuissa.net>
2025-12-31 20:08:01 -0600

    Move _paste-avoiding-double-git-clone to .dotfiles
    It's only used there and not documented here.
    
    See https://git.sr.ht/~razzi/.dotfiles/tree/main/item/fish_conf.d/keybindings.fish#L26

--- functions/_paste-avoiding-double-git-clone.fish
+++
@@ -1,8 +0,0 @@
-function _paste-avoiding-double-git-clone
-    set command (commandline | string trim)
-    set clipboard (fish_clipboard_paste | string collect -N)
-    if string match -qr '(clone-cd|clone-shallow-cd|git clone)' "$command"
-        set clipboard (echo $clipboard | string replace 'git clone ' '')
-    end
-    commandline -i -- $clipboard
-end
