e7cb4c3b78286d1c3c99ea3e5808f6936524b2ac
Razzi Abuissa <razzi@abuissa.net>
2026-01-24 01:01:43 -0600

    Make .git directory itself not in-git-repo

--- functions/in-git-repo.fish
+++ functions/in-git-repo.fish
@@ -1,4 +1,10 @@
 function in-git-repo --argument-names _dir
+    if equals (curdir) .git
+        # I don't consider the .git directory itself to be
+        # part of the repo, since you can't run git commands from it.
+        return 1
+    end
+
     set dir (default $_dir .)
     git -C $dir rev-parse --git-dir &> /dev/null
 end
