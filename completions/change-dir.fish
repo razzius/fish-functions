6745ee1017ce407fe19a2ad61fe87cde3aae74fd
Razzi Abuissa <razzi@abuissa.net>
2026-01-26 02:30:31 -0600

    Implement fuzzy folder matching for change-dir, and add more helper functions and inline test cases

--- completions/change-dir.fish
+++ completions/change-dir.fish
@@ -6,22 +6,50 @@ function __has_only_child_dir --argument-names dir
     equals $n_children 1 && is-dir $dir/*
 end
 
-function __complete_change_dir
-    set -l find_path (__completion_find_path_for_last_token cd)
-    set -l last_token (commandline -x)[-1]
-    set -l find_name (basename $last_token)
+# some test cases
+# cd ./ findpath ./ findname ''
+# cd ~/ findpath /Users/razzi/ findname ''
+# cd / findpath / findname ''
+# cd dir/another findpath dir/ findname another
+# cd .git/hook findpath .git/ findname hook
+# cd fofo findpath ./ findname fofo
+# cd - findpath ./ findname -
 
+function __get_find_name_for_path --argument-names path
+    echo $path | string replace -r '.*/' ''
+end
+
+function __escape_period --argument-names value
+    echo -- $value | string replace '.' '\.'
+end
+
+function __get_find_regex_for_token --argument-names token find_path
+    set -l find_name (__get_find_name_for_path $token)
+    set -l find_name_split (echo $find_name | string split '')
+    set -l find_name_escaped (__escape_period $find_name_split)
+    set -l find_path_escaped (__escape_period $find_path)
+    set -l find_regex $find_path_escaped(echo $find_name_escaped | string join '.*')'.*'
+    echo $find_regex
+end
+
+function __find_dirs_for_commandline_arg --argument-names find_path last_token
     if equals change-dir $last_token || endswith / $last_token
         set name_args -not -name '.*'
     else
-        set name_args -iname "*$find_name*"
+        set name_args -iregex (__get_find_regex_for_token $last_token $find_path)
     end
 
     set exclude_dir_itself_clause -not -name (basename $find_path)
 
-    set find_dirs find $find_path -maxdepth 1 -type d $exclude_dir_itself_clause $name_args
-    set find_links find $find_path -maxdepth 1 -type l -exec test -d {} \; $name_args -print
-    set dirs ($find_dirs | unexpand-home-tilde) ($find_links | unexpand-home-tilde)
+    find $find_path -maxdepth 1 -type d $exclude_dir_itself_clause $name_args
+    find $find_path -maxdepth 1 -type l -exec test -d {} \; $name_args -print
+end
+
+function __complete_change_dir
+    set -l find_path (__completion_find_path_for_last_token cd)
+    set -l last_token (commandline -x)[-1]
+
+    set -l dirs (__find_dirs_for_commandline_arg $find_path $last_token)
 
     for d in $dirs
         set path $d
@@ -31,6 +59,6 @@ function __complete_change_dir
             set path $path/*
         end
 
-        echo $path/ | string replace -r '^\./' ''
+        echo $path/ | string replace -r '^\./' '' | unexpand-home-tilde
     end
 end
