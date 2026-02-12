9919d1c384c28f0274a442ddac94c20a1195fb5c
Razzi Abuissa <razzi@abuissa.net>
2026-02-12 15:25:13 -0600

    Make find argument -maxdepth first because it was giving a warning

--- functions/file-exists.fish
+++ functions/file-exists.fish
@@ -1,5 +1,5 @@
 function file-exists --argument file
     set find_path (__completion_find_path_for_token $file)
     set filename (basename $file)
-    test (find $find_path -name $filename -maxdepth 1 | count) = 1
+    test (find $find_path -maxdepth 1 -name $filename | count) = 1
 end
