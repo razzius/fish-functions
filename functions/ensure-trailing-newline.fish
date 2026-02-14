8e1bf7ec72101ac1916f82a832e30d61a9f7ab9f
Razzi Abuissa <razzi@abuissa.net>
2026-02-14 01:04:27 -0600

    Better handling for ensure-trailing-newline (was this ever working?)

--- functions/ensure-trailing-newline.fish
+++ functions/ensure-trailing-newline.fish
@@ -1,9 +1,9 @@
 function ensure-trailing-newline --argument file
     if not file-exists $file
-        return
+        return 1
     end
 
-    if test -n (tail -c 1 $file)
+    if not equals (tail -c 1 $file | line-count) 1
         echo >> $file
     end
 end
