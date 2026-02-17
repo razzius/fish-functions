241701fef5d8f5ac754eac8da80e33d2573c41de
Razzi Abuissa <razzi@abuissa.net>
2026-02-17 11:58:20 -0600

    Default to origin for hub if current branch has no upstream

--- functions/hub.fish
+++ functions/hub.fish
@@ -1,10 +1,22 @@
-function hub --argument _remote
-    if string-empty $_remote
-        set remote (git rev-parse --abbrev-ref @{upstream} | cut -d / -f 1)
-    else
-        set remote $_remote
+function __get_remote_for_current_branch
+    if not string-empty $_remote
+        echo $_remote
+        return
     end
 
+    set upstream (git rev-parse --abbrev-ref @{upstream} 2> /dev/null)
+
+    if equals $status 0
+        echo $upstream | cut -d / -f 1
+        return
+    end
+
+    # No remote for current branch, assume "origin"
+    echo origin
+end
+
+function hub --argument _remote
+    set remote (__get_remote_for_current_branch $_remote)
     set url (git config --get remote.$remote.url)
     set https_url (echo $url | string replace -r 'git@(.+):' 'https://$1/')
     $BROWSER $https_url
