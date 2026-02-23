16b5b64cd3fdeb436a26cff67dcb87bcd5127915
Razzi Abuissa <razzi@abuissa.net>
2026-02-22 22:25:10 -0600

    Allow find-and-replace to handle multiline strings by using rg for both finding and replacing

--- functions/find-and-replace.fish
+++ functions/find-and-replace.fish
@@ -1,13 +1,24 @@
 function find-and-replace --argument-names find replace extension
+    if string-empty $extension
+        error 'find-and-replace: missing `extension` argument'
+        return 1
+    end
+
     set files (
         rg \
             --files-with-matches \
             --type-add "search:*.$extension" \
             --type=search \
             --color=never \
+            --multiline \
             -e $find
     )
     for f in $files
-        cat $f | string replace --all -- $find $replace | sponge $f
+        rg -e $find \
+            --passthru \
+            --multiline \
+            --no-line-number \
+            --color=never \
+            --replace $replace $f | sponge $f
     end
 end
