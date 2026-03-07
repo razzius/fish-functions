359ad3176cf3473f300e2749735edcd82e331d22
Razzi Abuissa <razzi@abuissa.net>
2026-03-07 13:08:20 -0600

    fish_indent functions/sync-repos.fish

--- functions/sync-repos.fish
+++ functions/sync-repos.fish
@@ -3,15 +3,15 @@ function sync-repos
         echo 'Put the repositories to sync in ~/.repos.txt'
         return 1
     end
-    
+
     for path in (cat ~/.repos.txt | grep -v '^#')
         set repo (echo $path | expand-home-tilde)
-        
+
         if not is-dir $repo
             echo $repo not present
             continue
         end
-        
+
         echo $path
         if not sync-repo $repo
             echo 'sync-repos: canceling'
