e33cc20e064c8e4d275b2f15ce57411a1a1b306a
Razzi Abuissa <razzi@abuissa.net>
2026-03-08 01:04:49 -0600

    Remove unnecessary startswith/endswith handling for prefix/suffix larger than string; they don't equal the string if they don't have the same length

--- functions/endswith.fish
+++ functions/endswith.fish
@@ -1,9 +1,5 @@
 function endswith --argument-names suffix string
     set suffix_len (echo -n $suffix | char-count)
-    if test $suffix_len -gt (echo -n $string | char-count)
-        return 1
-    end
-
     set slice (string sub -s -$suffix_len $string)
     equals $slice $suffix
 end
--- functions/startswith.fish
+++ functions/startswith.fish
@@ -1,10 +1,5 @@
 function startswith --argument prefix string
     set prefix_len (echo -n $prefix | char-count)
-
-    if test (echo -n $string | char-count) -lt $prefix_len
-        return 1
-    end
-
     set slice (string sub -l $prefix_len $string)
     equals $slice $prefix
 end
