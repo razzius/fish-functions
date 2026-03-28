f7304795f93a62ede1869ab5963869aced37686a
Razzi Abuissa <razzi@abuissa.net>
2026-03-28 13:25:07 -0500

    Make clean-unzip work with zipfiles and jarfiles

--- functions/clean-unzip.fish
+++ functions/clean-unzip.fish
@@ -1,13 +1,14 @@
 function clean-unzip --argument zipfile
-    if not test (echo $zipfile | string sub --start=-4) = .zip
-        echo (status function): argument must be a zipfile
+    set extension (echo $zipfile | string sub --start=-4)
+    if not contains $extension '.zip' '.jar'
+        echo (status function): argument must be a zipfile or jarfile
         return 1
     end
 
     if is-clean-zip $zipfile
         unzip $zipfile
     else
-        set folder_name (echo $zipfile | trim-right '.zip')
+        set folder_name (echo $zipfile | until-char .)
         set target (basename $folder_name)
         mkdir $target || return 1
         unzip $zipfile -d $target
