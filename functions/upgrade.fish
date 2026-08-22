70b16904685ef79c6bb86231b963ea71fba5e444
Razzi Abuissa <razzi@abuissa.net>
2026-08-22 17:51:56 -0500

    Make upgrade take argv for upgrading single packages

--- functions/upgrade.fish
+++ functions/upgrade.fish
@@ -1,7 +1,7 @@
 function upgrade
     if equals (uname) Darwin
-        brew upgrade -y
+        brew upgrade -y $argv
     else
-        sudo apt-get upgrade -y
+        sudo apt-get upgrade -y $argv
     end
 end
