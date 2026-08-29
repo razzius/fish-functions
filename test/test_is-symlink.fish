fcc09465ce8140efbfe6f999e3de564da6f2fd3d
Razzi Abuissa <razzi@abuissa.net>
2026-08-29 11:08:11 -0500

    Add test/test_is-symlink.fish

---
+++ test/test_is-symlink.fish
@@ -0,0 +1,32 @@
+function __cleanup
+    if string-empty $DEBUG
+        unsymlink file-symlink
+        rm file.txt
+        rmdir-.
+    end
+end
+
+function test_is-symlink
+    touch file.txt
+    symlink file.txt file-symlink
+
+    if not is-symlink file-symlink
+        error 'Expected is-symlink to report symlink'
+        return 1
+    end
+
+    if is-symlink file.txt
+        error 'Expected is-symlink to report not symlink'
+        return 1
+    end
+end
+
+function main
+    mkdir-cd test_symlink
+
+    run-tap-tests test_is-symlink
+
+    __cleanup
+end
+
+main
